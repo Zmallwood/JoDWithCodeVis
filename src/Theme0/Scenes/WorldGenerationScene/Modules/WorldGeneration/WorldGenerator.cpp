0d58e6c44d59216b2f70192ef275cd270958f6ea
Zmallwood <zmallwood@proton.me>
2024-10-13 15:34:00 +0200

    Change to camelCase.
    In WorldGenerator there was still some occurances of snake case in variables which i changed to camelCase.

--- src/Theme0/Scenes/WorldGenerationScene/Modules/WorldGeneration/WorldGenerator.cpp
+++ src/Theme0/Scenes/WorldGenerationScene/Modules/WorldGeneration/WorldGenerator.cpp
@@ -14,7 +14,9 @@
 namespace JoD {
 void WorldGenerator::GenerateWorld(Size worldAreaSize) const {
     _<World>().EnsureCreated(worldAreaSize);
+    
     auto worldArea = _<World>().GetCurrentWorldArea();
+    
     CreateRenderingResources(worldArea);
     GenerateGrass(worldArea);
     CalculateNormals(worldArea);
@@ -51,185 +53,224 @@ void WorldGenerator::CalculateNormals(
     std::shared_ptr<WorldArea> worldArea) const {
     auto tileSize = WorldViewConfiguration::k_tileSize;
     auto elevAmount = WorldViewConfiguration::k_elevAmount;
+    
     for (auto y = 0; y < worldArea->GetSize().height; y++){
         for (auto x = 0; x < worldArea->GetSize().width; x++){
-            auto tile_coord = Point2{x, y};
-            const auto tile = worldArea->GetTile(tile_coord.x, tile_coord.y);
-            const auto elev00 = static_cast<float>(tile->GetElevation());
+            
+            auto tileCoord = Point2 {x, y};
+            
+            const auto tile = worldArea->GetTile(tileCoord.x, tileCoord.y);
+            
+            auto elev00 = static_cast<float>(tile->GetElevation());
             auto elev10 = elev00;
             auto elev11 = elev00;
             auto elev01 = elev00;
-            const auto coord10 = tile_coord.Translate(1, 0);
-            const auto coord11 = tile_coord.Translate(1, 1);
-            const auto coord01 = tile_coord.Translate(0, 1);
+            
+            const auto coord10 = tileCoord.Translate(1, 0);
+            const auto coord11 = tileCoord.Translate(1, 1);
+            const auto coord01 = tileCoord.Translate(0, 1);
+            
             if (worldArea->IsValidCoordinate(coord10))
                 elev10 = worldArea->GetTile(coord10)->GetElevation();
+            
             if (worldArea->IsValidCoordinate(coord11))
                 elev11 = worldArea->GetTile(coord11)->GetElevation();
+            
             if (worldArea->IsValidCoordinate(coord01))
                 elev01 = worldArea->GetTile(coord01)->GetElevation();
-            auto x0 = tile_coord.x * tileSize;
+            
+            auto x0 = tileCoord.x * tileSize;
             auto y0 = elev00 * elevAmount;
-            auto z0 = tile_coord.y * tileSize;
-            auto x1 = tile_coord.x * tileSize+ tileSize;
+            auto z0 = tileCoord.y * tileSize;
+            auto x1 = tileCoord.x * tileSize+ tileSize;
             auto y1 = elev10 * elevAmount;
-            auto z1 = tile_coord.y * tileSize;
-            auto x2 = tile_coord.x * tileSize+ tileSize;
+            auto z1 = tileCoord.y * tileSize;
+            auto x2 = tileCoord.x * tileSize+ tileSize;
             auto y2 = elev11 * elevAmount;
-            auto z2 = tile_coord.y * tileSize+ tileSize;
-            auto x3 = tile_coord.x * tileSize;
+            auto z2 = tileCoord.y * tileSize+ tileSize;
+            auto x3 = tileCoord.x * tileSize;
             auto y3 = elev01 * elevAmount;
-            auto z3 = tile_coord.y * tileSize+ tileSize;
-            auto p0 = Point3F{x0, y0, z0};
-            auto p1 = Point3F{x1, y1, z1};
-            auto p3 = Point3F{x3, y3, z3};
+            auto z3 = tileCoord.y * tileSize+ tileSize;
+            auto p0 = Point3F {x0, y0, z0};
+            auto p1 = Point3F {x1, y1, z1};
+            auto p3 = Point3F {x3, y3, z3};
+            
             tile->CalculateNormal(p0, p1, p3);
         }
     }
     for (auto y = 0; y < worldArea->GetSize().height; y++){
         for (auto x = 0; x < worldArea->GetSize().width; x++){
-            Point2 tile_coord = {x, y};
-            const auto tile = worldArea->GetTile(tile_coord.x, tile_coord.y);
-            const auto elev00 = static_cast<float>(tile->GetElevation());
+            Point2 tileCoord = {x, y};
+            
+            const auto tile = worldArea->GetTile(tileCoord.x, tileCoord.y);
+            
+            auto elev00 = static_cast<float>(tile->GetElevation());
             auto elev10 = elev00;
             auto elev11 = elev00;
             auto elev01 = elev00;
-            const auto coord10 = tile_coord.Translate(1, 0);
-            const auto coord11 = tile_coord.Translate(1, 1);
-            const auto coord01 = tile_coord.Translate(0, 1);
+            
+            const auto coord10 = tileCoord.Translate(1, 0);
+            const auto coord11 = tileCoord.Translate(1, 1);
+            const auto coord01 = tileCoord.Translate(0, 1);
+            
             if (worldArea->IsValidCoordinate(coord10))
                 elev10 = worldArea->GetTile(coord10)->GetElevation();
+            
             if (worldArea->IsValidCoordinate(coord11))
                 elev11 = worldArea->GetTile(coord11)->GetElevation();
+            
             if (worldArea->IsValidCoordinate(coord01))
                 elev01 = worldArea->GetTile(coord01)->GetElevation();
-            auto x0 = tile_coord.x * tileSize;
+            
+            auto x0 = tileCoord.x * tileSize;
             auto y0 = elev00 * elevAmount;
-            auto z0 = tile_coord.y * tileSize;
-            auto x1 = tile_coord.x * tileSize+ tileSize;
+            auto z0 = tileCoord.y * tileSize;
+            auto x1 = tileCoord.x * tileSize+ tileSize;
             auto y1 = elev10 * elevAmount;
-            auto z1 = tile_coord.y * tileSize;
-            auto x2 = tile_coord.x * tileSize+ tileSize;
+            auto z1 = tileCoord.y * tileSize;
+            auto x2 = tileCoord.x * tileSize+ tileSize;
             auto y2 = elev11 * elevAmount;
-            auto z2 = tile_coord.y * tileSize+ tileSize;
-            auto x3 = tile_coord.x * tileSize;
+            auto z2 = tileCoord.y * tileSize+ tileSize;
+            auto x3 = tileCoord.x * tileSize;
             auto y3 = elev01 * elevAmount;
-            auto z3 = tile_coord.y * tileSize+ tileSize;
+            auto z3 = tileCoord.y * tileSize+ tileSize;
+            
             Vertex3F v0;
             Vertex3F v1;
             Vertex3F v2;
             Vertex3F v3;
+            
             v0.position = {x0, y0, z0};
             v1.position = {x1, y1, z1};
             v2.position = {x2, y2, z2};
             v3.position = {x3, y3, z3};
+            
             v0.uv = {0.0f, 0.0f};
             v1.uv = {1.0f, 0.0f};
             v2.uv = {1.0f, 1.0f};
             v3.uv = {0.0f, 1.0f};
-            auto red_variation = tile->GetRedVariation();
-            auto green_variation = tile->GetGreenVariation();
-            auto blue_variation = tile->GetBlueVariation();
-            auto red_variation_east = red_variation;
-            auto green_variation_east = green_variation;
-            auto blue_variation_east = blue_variation;
-            auto red_variation_south = red_variation;
-            auto green_variation_south = green_variation;
-            auto blue_variation_south = blue_variation;
-            auto red_variation_south_east = red_variation;
-            auto green_variation_south_east = green_variation;
-            auto blue_variation_south_east = blue_variation;
-            if (tile_coord.x < worldArea->GetSize().width - 1){
-                red_variation_east =
+            
+            auto redVariation = tile->GetRedVariation();
+            auto greenVariation = tile->GetGreenVariation();
+            auto blueVariation = tile->GetBlueVariation();
+            auto redVariationEast = redVariation;
+            auto greenVariationEast = greenVariation;
+            auto blueVariationEast = blueVariation;
+            auto redVariationSouth = redVariation;
+            auto greenVariationSouth = greenVariation;
+            auto blueVariationSouth = blueVariation;
+            auto redVariationSouthEast = redVariation;
+            auto greenVariationSouthEast = greenVariation;
+            auto blueVariationSouthEast = blueVariation;
+            
+            if (tileCoord.x < worldArea->GetSize().width - 1){
+                redVariationEast =
                     worldArea->GetTile(
-                        {tile_coord.x + 1,
-                         tile_coord.y})->GetRedVariation();
-                green_variation_east = worldArea->GetTile(
-                    {tile_coord.x + 1,
-                     tile_coord.y})
-                                       ->GetGreenVariation();
-                blue_variation_east =
+                        {tileCoord.x + 1,
+                         tileCoord.y})->GetRedVariation();
+                
+                greenVariationEast = worldArea->GetTile(
+                    {tileCoord.x + 1,
+                     tileCoord.y})
+                                     ->GetGreenVariation();
+                
+                blueVariationEast =
                     worldArea->GetTile(
-                        {tile_coord.x + 1,
-                         tile_coord.y})->GetBlueVariation();
+                        {tileCoord.x + 1,
+                         tileCoord.y})->GetBlueVariation();
             }
-            if (tile_coord.y < worldArea->GetSize().height - 1){
-                red_variation_south =
+            
+            if (tileCoord.y < worldArea->GetSize().height - 1){
+                redVariationSouth =
                     worldArea->GetTile(
-                        {tile_coord.x,
-                         tile_coord.y +
+                        {tileCoord.x,
+                         tileCoord.y +
                          1})->GetRedVariation();
-                green_variation_south = worldArea->GetTile(
-                    {tile_coord.x,
-                     tile_coord.y +
+                
+                greenVariationSouth = worldArea->GetTile(
+                    {tileCoord.x,
+                     tileCoord.y +
                      1})
-                                        ->GetGreenVariation();
-                blue_variation_south =
+                                      ->GetGreenVariation();
+                
+                blueVariationSouth=
                     worldArea->GetTile(
-                        {tile_coord.x,
-                         tile_coord.y +
+                        {tileCoord.x,
+                         tileCoord.y +
                          1})->GetBlueVariation();
             }
-            if (tile_coord.x < worldArea->GetSize().width - 1 &&
-                tile_coord.y < worldArea->GetSize().height - 1){
-                red_variation_south_east =
-                    worldArea->GetTile({tile_coord.x + 1, tile_coord.y + 1})
+            
+            if (tileCoord.x < worldArea->GetSize().width - 1 &&
+                tileCoord.y < worldArea->GetSize().height - 1){
+                redVariationSouthEast =
+                    worldArea->GetTile({tileCoord.x + 1, tileCoord.y + 1})
                     ->GetRedVariation();
-                green_variation_south_east =
-                    worldArea->GetTile({tile_coord.x + 1, tile_coord.y + 1})
+                
+                greenVariationSouthEast =
+                    worldArea->GetTile({tileCoord.x + 1, tileCoord.y + 1})
                     ->GetGreenVariation();
-                blue_variation_south_east =
-                    worldArea->GetTile({tile_coord.x + 1, tile_coord.y + 1})
+                
+                blueVariationSouthEast =
+                    worldArea->GetTile({tileCoord.x + 1, tileCoord.y + 1})
                     ->GetBlueVariation();
             }
+            
             if (tile->GetGroundType() == Hash("GroundWater")){
-                red_variation = red_variation_east = red_variation_south =
-                    red_variation_south_east = 0.0f;
-                green_variation = green_variation_east =
-                    green_variation_south =
-                        green_variation_south_east = 0.0f;
-                blue_variation = blue_variation_east =
-                    blue_variation_south =
-                        blue_variation_south_east = 0.0f;
+                redVariation = redVariationEast = redVariationSouth =
+                    redVariationSouthEast = 0.0f;
+                
+                greenVariation = greenVariationEast =
+                    greenVariationSouth =
+                        greenVariationSouthEast = 0.0f;
+                
+                blueVariation = blueVariationEast =
+                    blueVariationSouth =
+                        blueVariationSouthEast = 0.0f;
             }
+            
             v0.color = {
-                1.0f - red_variation, 1.0f - green_variation,
-                1.0f - blue_variation, 1.0f};
+                1.0f - redVariation, 1.0f - greenVariation,
+                1.0f - blueVariation, 1.0f};
+            
             v1.color = {
-                1.0f - red_variation_east, 1.0f - green_variation_east,
-                1.0f - blue_variation_east, 1.0f};
+                1.0f - redVariationEast, 1.0f - greenVariationEast,
+                1.0f - blueVariationEast, 1.0f};
+            
             v2.color = {
-                1.0f - red_variation_south_east,
-                1.0f - green_variation_south_east,
-                1.0f - blue_variation_south_east, 1.0f};
+                1.0f - redVariationSouthEast,
+                1.0f - greenVariationSouthEast,
+                1.0f - blueVariationSouthEast, 1.0f};
+            
             v3.color = {
-                1.0f - red_variation_south, 1.0f - green_variation_south,
-                1.0f - blue_variation_south, 1.0f};
+                1.0f - redVariationSouth, 1.0f - greenVariationSouth,
+                1.0f - blueVariationSouth, 1.0f};
+            
             tile->SetVertex0(v0);
             tile->SetVertex1(v1);
             tile->SetVertex2(v2);
             tile->SetVertex3(v3);
+            
             auto normal00 = tile->GetNormal();
             auto normal10 = normal00;
             auto normal11 = normal00;
             auto normal01 = normal00;
+            
             if (worldArea->IsValidCoordinate(coord10))
                 normal10 = worldArea->GetTile(coord10)->GetNormal();
+            
             if (worldArea->IsValidCoordinate(coord11))
                 normal11 = worldArea->GetTile(coord11)->GetNormal();
+            
             if (worldArea->IsValidCoordinate(coord01))
                 normal01 = worldArea->GetTile(coord01)->GetNormal();
-            GLuint rendid_ground;
-//            if (false == updateExistingTiles){
-//                rendid_ground = RendererTiles::Get()->NewTile();
-//            }
-//            else{
-            rendid_ground = tile->GetRID();
-//            }
+            
+            auto ridGround = tile->GetRID();
+            
             _<GroundRenderer>().SetGeometryTile(
-                rendid_ground, v0, v1, v2, v3, normal00, normal10, normal11,
+                ridGround, v0, v1, v2, v3, normal00, normal10, normal11,
                 normal01);
-            tile->SetRID(rendid_ground);
+            
+            tile->SetRID(ridGround);
         }
     }
 }
